cfd9bc22a3a08dddabf25012e5f9e984137b04ec
Joël R. Langlois <joel.r.langlois@gmail.com>
2022-09-10 11:40:13 -0400

    Added OpenGL detail scanning.

--- demo/source/MainModule.h
+++ demo/source/MainModule.h
@@ -7,7 +7,7 @@
     Enables or disables setting the application up for OpenGL.
 */
 #ifndef USE_OPENGL
-    #define USE_OPENGL 0 // JUCE_MODULE_AVAILABLE_juce_opengl
+    #define USE_OPENGL JUCE_MODULE_AVAILABLE_juce_opengl
 #endif
 
 #include "demos/ImageDemo.h"
