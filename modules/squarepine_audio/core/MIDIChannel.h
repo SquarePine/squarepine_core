6f55031921636e12abf67d5df5771d3c6894863d
Joël R. Langlois <joel.r.langlois@gmail.com>
2022-09-29 17:25:24 -0400

    Fixed MIDIChannel

--- modules/squarepine_audio/core/MIDIChannel.h
+++ modules/squarepine_audio/core/MIDIChannel.h
@@ -14,21 +14,20 @@ struct MIDIChannel final
 
         It's up to the caller to be sure this is valid!
     */
-    explicit MIDIChannel (int channelNumber1to16) noexcept :
+    explicit constexpr MIDIChannel (int channelNumber1to16) noexcept :
         channel (std::clamp (channelNumber1to16, 1, 16))
     {
-        jassert (channelNumber1to16 >= 1 && channelNumber1to16 <= 16);
     }
 
     //==============================================================================
     /** @returns the currently held MIDI channel value, which may be '0' (invalid). */
-    constexpr int get() const noexcept              { return channel; }
+    [[nodiscard]] constexpr int get() const noexcept                { return channel; }
 
     /** @returns true if this MIDI channel is valid (is a channel from 1 to 16). */
-    constexpr bool isValid() const noexcept         { return channel >= 1 && channel <= 16; }
+    [[nodiscard]] constexpr bool isValid() const noexcept           { return channel >= 1 && channel <= 16; }
 
     /** @returns true if this MIDI channel is channel 10. */
-    constexpr bool isPercussion() const noexcept    { return channel == 10; }
+    [[nodiscard]] constexpr bool isPercussion() const noexcept      { return channel == 10; }
 
     //==============================================================================
     /** @returns true if this channel represents the same channel as the other one. */
@@ -47,11 +46,11 @@ struct MIDIChannel final
 
     //==============================================================================
     /** Prefix increment. */
-    MIDIChannel& operator++()                                   { ++channel; return *this;  }
+    MIDIChannel& operator++()                                   { *this = MIDIChannel (channel++); return *this;  }
     /** Postfix increment. */
     MIDIChannel operator++ (int)                                { auto old = *this; operator++(); return old; }
     /** Prefix decrement. */
-    MIDIChannel& operator--()                                   { --channel; return *this;  }
+    MIDIChannel& operator--()                                   { *this = MIDIChannel (channel--); return *this;  }
     /** Postfix decrement. */
     MIDIChannel operator-- (int)                                { auto old = *this; operator--(); return old; }
 
@@ -59,25 +58,25 @@ struct MIDIChannel final
     MIDIChannel operator+ (MIDIChannel other) const noexcept    { return MIDIChannel (channel + other.channel); }
 
     /** Adds another MIDI channel's channel to this one. */
-    MIDIChannel& operator+= (MIDIChannel other) noexcept        { channel += other.channel; return *this; }
+    MIDIChannel& operator+= (MIDIChannel other) noexcept        { *this = MIDIChannel (channel + other.channel); return *this; }
 
     /** Subtracts one MIDI channels from another. */
     MIDIChannel operator- (MIDIChannel other) const noexcept    { return MIDIChannel (channel - other.channel); }
 
     /** Subtracts another MIDI channel's channel to this one. */
-    MIDIChannel& operator-= (MIDIChannel other) noexcept        { channel -= other.channel; return *this; }
+    MIDIChannel& operator-= (MIDIChannel other) noexcept        { *this = MIDIChannel (channel - other.channel); return *this; }
 
     /** Multiplies two MIDI channels together */
     MIDIChannel operator* (MIDIChannel other) const noexcept    { return MIDIChannel (channel * other.channel); }
 
     /** Multiplies another MIDI channel's channel to this one. */
-    MIDIChannel& operator*= (MIDIChannel other) noexcept        { *this = *this * other; return *this; }
+    MIDIChannel& operator*= (MIDIChannel other) noexcept        { *this = MIDIChannel (channel * other.channel); return *this; }
 
     /** Divides one MIDI channel by another. */
     MIDIChannel operator/ (MIDIChannel other) const noexcept    { return MIDIChannel (channel / other.channel); }
 
     /** Divides this MIDI channel by another. */
-    MIDIChannel& operator/= (MIDIChannel other) noexcept        { *this = *this / other; return *this; }
+    MIDIChannel& operator/= (MIDIChannel other) noexcept        { *this = MIDIChannel (channel / other.channel); return *this; }
 
     /** @returns a MIDI channel multiplied by a given value. */
     MIDIChannel operator* (int multiplier) const noexcept       { return MIDIChannel (channel * multiplier); }
@@ -86,10 +85,10 @@ struct MIDIChannel final
     MIDIChannel operator/ (int divisor) const noexcept          { return MIDIChannel (channel / divisor); }
 
     /** Multiplies the MIDI channel by some multiplier. */
-    MIDIChannel& operator*= (int multiplier) noexcept           { channel *= multiplier; return *this; }
+    MIDIChannel& operator*= (int multiplier) noexcept           { *this = MIDIChannel (channel * multiplier); return *this; }
 
     /** Divides the MIDI channel by some divisor. */
-    MIDIChannel& operator/= (int divisor) noexcept              { channel /= divisor; return *this; }
+    MIDIChannel& operator/= (int divisor) noexcept              { *this = MIDIChannel (channel / divisor); return *this; }
 
 private:
     //==============================================================================
