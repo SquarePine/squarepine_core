3df4295c24ee2e055d83e8b1eefc1a5684a5fe60
Joël R. Langlois <joel.r.langlois@gmail.com>
2024-10-16 13:30:10 -0400

    Animation: lots of particle system work.

--- modules/squarepine_animation/particles/ParticleSystem.h
+++ modules/squarepine_animation/particles/ParticleSystem.h
@@ -1,10 +1,3 @@
-// Random utility function
-inline float getRandomFloat (float min, float max)
-{
-    const auto n = Random::getSystemRandom().nextFloat();
-    return lerp (min, max, n);
-}
-
 /** Base Particle Class. */
 class Particle
 {
@@ -12,124 +5,203 @@ public:
     /** */
     Particle (juce::Rectangle<float> bounds,
               float velocityX, float velocityY,
-              Colour colour)
+              Colour colour,
+              std::optional<double> lifetimeSeconds)
     {
-        consts.bounds = bounds;
+        consts.bounds   = bounds;
         consts.velocity = { velocityX, velocityY };
-        consts.colour = colour;
-        changeable = consts;
+        consts.colour   = colour;
+        changeable      = consts;
+        lifetime        = lifetimeSeconds;
     }
 
     /** */
     Particle (float x, float y, float size,
               float velocityX, float velocityY,
-              Colour c) :
-        Particle ({ x, y, size, size }, velocityX, velocityY, c)
+              Colour colour,
+              std::optional<double> lifetimeSeconds) :
+        Particle ({ x, y, size, size },
+                  velocityX, velocityY,
+                  colour, lifetimeSeconds)
     {
     }
 
     /** */
     virtual ~Particle() = default;
 
-    /** @returns the gravity in m/s². */
-    virtual float getGravity() const { return 9.80665f; }
+    //==============================================================================
+    /** @returns the gravity in pixels per update. */
+    virtual float getGravity() const { return 1.0f; }
 
-    /** Virtual method for updating the particle's position. */
-    virtual void update()
+    /** Updates the particle's position and velocity as per the provided gravity (in px/update).
+
+        @see getGravity
+    */
+    virtual void update (const Time& currentTime)
     {
+        if (! started && lifetime.has_value())
+            startTime = currentTime;
+
+        started = true;
         changeable.velocity.y += getGravity();
         changeable.bounds += changeable.velocity;
     }
 
     /** */
-    enum class EndingMethod
-    {
-        reset,
-        remove
-    };
-
-    /** */
-    virtual std::optional<EndingMethod> shouldEnd (juce::Rectangle<float> area) const
+    virtual bool shouldEnd (const Time& currentTime, const juce::Rectangle<float>& area) const
     {
         if (area.contains (changeable.bounds))
-            return {};
+            return false;
 
-        return { EndingMethod::reset };
+        if (started
+            && lifetime.has_value()
+            && (startTime + RelativeTime (*lifetime)) < currentTime)
+        {
+            return false;
+        }
+
+        return true;
     }
 
-    /** Method to draw the particle. */
+    /** Resets the particle's bounds, velocity, and colour to
+        those that were set in the constructor.
+    */
+    virtual void reset()
+    {
+        changeable = consts;
+        startTime = Time();
+        started = false;
+    }
+
+    /** Your derived particle class must fill this to draw as needed. */
     virtual void paint (Graphics&) = 0;
 
-    /** */
-    virtual void reset() { changeable = consts; }
+    //==============================================================================
+    /** @returns the lifetime of the particle, as set by setLifetimeSeconds(). */
+    [[nodiscard]] std::optional<double> getLifetimeSeconds() const noexcept { return lifetime; }
+
+    /** @returns the original bounds as set in the constructor. */
+    [[nodiscard]] const juce::Rectangle<float>& getSourceBounds() const noexcept { return consts.bounds; }
+    /** @returns the original velocity as set in the constructor. */
+    [[nodiscard]] const juce::Point<float>& getSourceVelocity() const noexcept { return consts.velocity; }
+    /** @returns the original colour as set in the constructor. */
+    [[nodiscard]] Colour getSourceColour() const noexcept { return consts.colour; }
+
+    /** @returns the current bounds, which may differ from the one set in the constructor.
+
+        This would occur if the particle had its bounds overridden in the update() call.
+    */
+    [[nodiscard]] const juce::Rectangle<float>& getActiveBounds() const noexcept { return changeable.bounds; }
+
+    /** @returns the current velocity (X, Y), which may differ from the one set in the constructor.
+
+        This would occur if the particle had its velocity overridden in the update() call.
+    */
+    [[nodiscard]] const juce::Point<float>& getActiveVelocity() const noexcept { return changeable.velocity; }
+
+    /** @returns the current colour, which may differ from the one set in the constructor.
+
+        This would occur if the particle had its Colour overridden in the update() call.
+    */
+    [[nodiscard]] Colour getActiveColour() const noexcept { return changeable.colour; }
+
+    //==============================================================================
+    /** @returns a random float between the given values. */
+    static float getRandomFloat (float min, float max)
+    {
+        const auto n = Random::getSystemRandom().nextFloat();
+        return lerp (min, max, n);
+    }
 
     /** */
-    juce::Rectangle<float> getSourceBounds() const noexcept { return consts.bounds; }
-    /** */
-    juce::Point<float> getSourceVelocity() const noexcept { return consts.velocity; }
-    /** */
-    Colour getSourceColour() const noexcept { return consts.colour; }
+    static juce::Rectangle<float> createRectangleWithRandomWidth (float min, float max, float height)
+    {
+        return { height, getRandomFloat (min, max) };
+    }
 
     /** */
-    juce::Rectangle<float> getActiveBounds() const noexcept { return changeable.bounds; }
+    static juce::Rectangle<float> createRectangleWithRandomHeight (float min, float max, float width)
+    {
+        return { getRandomFloat (min, max), width };
+    }
+
     /** */
-    juce::Point<float> getActiveVelocity() const noexcept { return changeable.velocity; }
-    /** */
-    Colour getActiveColour() const noexcept { return changeable.colour; }
+    static juce::Rectangle<float> createRectangleWithRandomSize (float min, float max)
+    {
+        const auto s = getRandomFloat (min, max);
+        return { s, s };
+    }
 
 protected:
+    //==============================================================================
+   #if JUCE_MSVC
+    #pragma pack (push, 1)
+   #endif
+
+    /** */
     struct Details final
     {
         juce::Rectangle<float> bounds;
         juce::Point<float> velocity;
         Colour colour;
-    };
+    } JUCE_PACKED;
+
+   #if JUCE_MSVC
+    #pragma pack (pop)
+   #endif
 
     Details changeable;
 
 private:
+    //==============================================================================
     Details consts;
+    std::optional<double> lifetime;
+    bool started = false;
+    Time startTime;
 
+    //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Particle)
 };
 
+//==============================================================================
 class RainParticle final : public Particle
 {
 public:
-    RainParticle (float x, float y) :
-        Particle (x, y, getRandomFloat (5.0f, 10.0f),
+    RainParticle (float x, float y, std::optional<double> lifetimeSeconds = {}) :
+        Particle (createRectangleWithRandomHeight (5.0f, 10.0f, 1.0f).withPosition (x, y),
                   0.0f, getRandomFloat (5.0f, 15.0f),
-                  juce::Colours::blue.withAlpha (0.5f))
+                  juce::Colours::blue.withAlpha (0.5f),
+                  lifetimeSeconds)
     {
     }
 
     void paint (Graphics& g) override
     {
         g.setColour (getActiveColour());
-        g.fillRect (getActiveBounds());
+        g.fillRect (getActiveBounds().withWidth (1.0f));
     }
 
 private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RainParticle)
 };
 
-// SnowParticle Class
+//==============================================================================
 class SnowParticle final : public Particle
 {
 public:
-    SnowParticle (float x, float y) :
-        Particle (x, y, getRandomFloat (3.0f, 5.0f),
+    SnowParticle (float x, float y, std::optional<double> lifetimeSeconds = {}) :
+        Particle (createRectangleWithRandomSize (3.0f, 5.0f).withPosition (x, y),
                   getRandomFloat (-2.0f, 2.0f), getRandomFloat (1.0f, 3.0f),
-                  juce::Colours::white)
+                  juce::Colours::white.withAlpha (0.5f), lifetimeSeconds)
     {
     }
 
-    float getGravity() const override { return MathConstants<float>::pi; }
+    float getGravity() const override { return 0.03f; }
 
-    void update() override
+    void update (const Time& currentTime) override
     {
         changeable.velocity.x += getRandomFloat (-0.5f, 0.5f);
-        Particle::update();
+        Particle::update (currentTime);
     }
 
     void paint (Graphics& g) override
@@ -142,6 +214,114 @@ private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SnowParticle)
 };
 
+//==============================================================================
+/**
+*/
+class ParticleEmitter final
+{
+public:
+    /** Constructor.
+
+        @param shouldRemoveParticlesOnEnd
+    */
+    ParticleEmitter (bool shouldRemoveParticlesOnEnd = false) :
+        shouldRemoveOnEnd (shouldRemoveParticlesOnEnd)
+    {
+    }
+
+    //==============================================================================
+    /** @returns */
+    [[nodiscard]] bool removesParticlesOnEnd() const noexcept { return shouldRemoveOnEnd; }
+
+    //==============================================================================
+    /** Appends a new particle to the list of particles.
+
+        Note that this particle will be deleted by the particle emitter when it is removed,
+        so be careful not to delete it somewhere else.
+
+        Also be careful not to add the same particle to the emitter more than once,
+        as this will obviously cause deletion of dangling pointers.
+
+        @param newObject    The new object to add to the array.
+        @returns            The new object that was added.
+    */
+    Particle* add (Particle* particle) { return particles.add (particle); }
+
+    /** Appends a new particle to the list of particles.
+
+        Note that this particle will be deleted by the particle emitter when it is removed,
+        so be careful not to delete it somewhere else.
+
+        Also be careful not to add the same particle to the emitter more than once,
+        as this will obviously cause deletion of dangling pointers.
+
+        @param newObject    The new object to add to the array.
+        @returns            The new object that was added.
+    */
+    Particle* add (std::unique_ptr<Particle> newParticle) { return add (newParticle.release()); }
+
+    /** Removes a specified particle.
+
+        If the particle isn't found, no action is taken.
+
+        @param particle     The particle to try to remove.
+        @param deleteObject Whether to delete the particle (if it's found).
+    */
+    void remove (Particle* particle, bool deleteObject = true)
+    {
+        particles.removeObject (particle, deleteObject);
+    }
+
+    /** @returns the number of particles currently stored in this particle emitter. */
+    [[nodiscard]] int getNumParticles() const noexcept { return particles.size(); }
+
+    /** Reduces the amount of storage being used by the array of particles.
+
+        Arrays typically allocate slightly more storage than they need, and after
+        removing elements, they may have quite a lot of unused space allocated.
+        This method will reduce the amount of allocated storage to a minimum.
+    */
+    void minimiseStorageOverheads() noexcept { particles.minimiseStorageOverheads(); }
+
+    //==============================================================================
+    /** */
+    void update (const Time& currentTime, juce::Rectangle<float> paintingArea)
+    {
+        Array<Particle*> toResetOrRemove;
+
+        for (auto& particle : particles)
+        {
+            particle->update (currentTime);
+
+            if (particle->shouldEnd (currentTime, paintingArea))
+                toResetOrRemove.add (particle);
+        }
+
+        if (shouldRemoveOnEnd)
+            for (auto& particle : toResetOrRemove)
+                remove (particle);
+        else
+            for (auto& particle : toResetOrRemove)
+                particle->reset();
+    }
+
+    /** */
+    void paint (Graphics& g)
+    {
+        for (auto& particle : particles)
+            particle->paint (g);
+    }
+
+private:
+    //==============================================================================
+    const bool shouldRemoveOnEnd = false;
+    OwnedArray<Particle> particles;
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ParticleEmitter)
+};
+
+//==============================================================================
 class ParticleSystemComponent : public Component,
                                 private Timer
 {
@@ -149,63 +329,32 @@ public:
     ParticleSystemComponent()
     {
         for (int i = 0; i < 1000; ++i)
-            add (new RainParticle (getRandomFloat (0.0f, 800.0f), getRandomFloat (0.0f, 600.0f)));
+            snowEmitter.add (new RainParticle (Particle::getRandomFloat (0.0f, 800.0f),
+                                               Particle::getRandomFloat (0.0f, 600.0f)));
 
         for (int i = 0; i < 500; ++i)
-            add (new SnowParticle (getRandomFloat (0.0f, 800.0f), getRandomFloat (0.0f, 600.0f)));
+            rainEmitter.add (new SnowParticle (Particle::getRandomFloat (0.0f, 800.0f),
+                                               Particle::getRandomFloat (0.0f, 600.0f)));
 
         startTimerHz (60);
     }
 
     void paint (Graphics& g) override
     {
-        for (auto& particle : particles)
-            particle->paint (g);
-    }
-
-    void resized() override
-    {
-    }
-
-    void add (Particle* particle)
-    {
-        particles.add (particle);
-    }
-
-    void remove (Particle* particle)
-    {
-        particles.removeObject (particle);
+        snowEmitter.paint (g);
+        rainEmitter.paint (g);
     }
 
 private:
-    OwnedArray<Particle> particles;
+    ParticleEmitter snowEmitter, rainEmitter;
 
     void timerCallback() override
     {
         const auto b = getLocalBounds().toFloat();
+        const auto ct = Time::getCurrentTime();
 
-        Array<Particle*> toRemove, toReset;
-
-        for (auto& particle : particles)
-        {
-            particle->update();
-
-            if (const auto result = particle->shouldEnd (b); result.has_value())
-            {
-                switch (*result)
-                {
-                    case Particle::EndingMethod::reset:     toReset.add (particle); break;
-                    case Particle::EndingMethod::remove:    toRemove.add (particle); break;
-                    default: jassertfalse; break;
-                };
-            }
-        }
-
-        for (auto& particle : toRemove)
-            particles.removeObject (particle);
-
-        for (auto& particle : toReset)
-            particle->reset();
+        snowEmitter.update (ct, b);
+        rainEmitter.update (ct, b);
 
         repaint();
     }
