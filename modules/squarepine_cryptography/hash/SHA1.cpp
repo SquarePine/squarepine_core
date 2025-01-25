dd2b50875c8e2791eb3c0b7bc8195bcedfbcd363
Joël R. Langlois <joel.r.langlois@gmail.com>
2025-01-25 16:42:13 -0500

    Cryptography: some preliminary SHA1 work.

--- modules/squarepine_cryptography/hash/SHA1.cpp
+++ modules/squarepine_cryptography/hash/SHA1.cpp
@@ -1,85 +1,204 @@
-struct SHA1Processor
+//==============================================================================
+struct SHA1Processor final
 {
-    SHA1Processor()
+    using ResultArray = std::array<uint8, 20>;
+    using State = std::array<uint32, 5>;
+
+    SHA1Processor() noexcept = default;
+
+    ResultArray processStream (InputStream& input, int64 numBytesToRead)
     {
-        // have to copy this data manually, as VS2013 doesn't support member array initialisers
-        const uint32 initialState[] = { 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0 };
-        std::memcpy (state, initialState, sizeof (state));
-    }
+        if (numBytesToRead < 0)
+            numBytesToRead = std::numeric_limits<int64>::max();
 
-    // expects 256 bytes of data
-    void processFullBlock (const void* data) noexcept
-    {
-        uint32 s[5], w[80];
-        std::memcpy (s, state, sizeof (s));
-        std::memset (w, 0, sizeof (w));
-
-        //Extend the sixteen 32-bit words into eighty 32-bit words:
-        auto* source = static_cast<const uint8_t*> (data);
-        (void) source;
-
-        for (size_t i = 16; i < 79; ++i)
-            w[i] = rotateLeft (w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16], 1);
-
-        auto a = state[0];
-        auto b = state[1];
-        auto c = state[2];
-        auto d = state[3];
-        auto e = state[4];
-
-        for (size_t i = 0; i < 80; ++i)
+        ResultArray buffer;
+        for (;;)
         {
-            uint32 k = 0, f = 0;
+            buffer.fill (0);
+            auto bytesRead = input.read (buffer.data(), (int) jmin (numBytesToRead, (int64) buffer.size()));
 
-            if (isPositiveAndBelow (i, 19))
+            if (bytesRead < (int) buffer.size())
             {
-                f = (b & c) | ((! b) & d);
-                k = 0x5a827999;
-            }
-            else if (i >= 20 && i <= 39)
-            {
-                f = b ^ c ^ d;
-                k = 0x6ed9eba1;
-            }
-            else if (i >= 40 && i <= 59)
-            {
-                f = (b & c) | (b & d) | (c & d);
-                k = 0x8f1bbcdc;
-            }
-            else if (i >= 60 && i <= 79)
-            {
-                f = b ^ c ^ d;
-                k = 0xca62c1d6;
+                processFinalBlock (buffer.data(), (uint32) bytesRead);
+                break;
             }
 
-            auto temp = rotateLeft (a, 5) + f + e + k + w[i];
-            e = d;
-            d = c;
-            c = rotateLeft (b, 30);
-            b = a;
-            a = temp;
+            numBytesToRead -= (int64) buffer.size();
+            processFullBlock (buffer.data(), buffer.size());
         }
 
-        state[0] += a;
-        state[1] += b;
-        state[2] += c;
-        state[3] += d;
-        state[4] += e;
+        return copyResult();
+    }
 
-        length += 64;
+private:
+    State state = { 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0 };
+    uint64 length = 0;
+
+    static constexpr uint32 ch (uint32 x, uint32 y, uint32 z) noexcept  { return z ^ ((y ^ z) & x); }
+    static constexpr uint32 maj (uint32 x, uint32 y, uint32 z) noexcept { return y ^ ((y ^ z) & (x ^ y)); }
+    static constexpr uint32 s0 (uint32 x) noexcept                      { return std::rotr (x, 7)  ^ std::rotr (x, 18) ^ (x >> 3); }
+    static constexpr uint32 s1 (uint32 x) noexcept                      { return std::rotr (x, 17) ^ std::rotr (x, 19) ^ (x >> 10); }
+    static constexpr uint32 S0 (uint32 x) noexcept                      { return std::rotr (x, 2)  ^ std::rotr (x, 13) ^ std::rotr (x, 22); }
+    static constexpr uint32 S1 (uint32 x) noexcept                      { return std::rotr (x, 6)  ^ std::rotr (x, 11) ^ std::rotr (x, 25); }
+
+    ResultArray copyResult() const noexcept
+    {
+        ResultArray result;
+        auto* rawData = result.data();
+
+        for (auto s : state)
+        {
+            *rawData++ = (uint8) (s >> 24);
+            *rawData++ = (uint8) (s >> 16);
+            *rawData++ = (uint8) (s >> 8);
+            *rawData++ = (uint8) s;
+        }
+
+        return result;
+    }
+
+    // expects 20 bytes of data
+    void processFullBlock (const void* data, uint64 numBytes) noexcept
+    {
+        uint32 W[80]        = { 0 };
+        uint8 datatail[128] = { 0 };
+        uint64 databits     = numBytes * 8;
+        uint32 loopcount    = (numBytes + 8) / 64 + 1;
+        uint32 tailbytes    = 64 * loopcount - numBytes;
+
+        uint32 a = 0,
+               b = 0,
+               c = 0,
+               d = 0,
+               e = 0,
+               k = 0,
+               temp = 0;
+
+        /** Pre-processing of data tail (includes padding to fill out 512-bit chunk):
+            Add bit '1' to end of message (big-endian)
+            Add 64-bit message length in bits at very end (big-endian)
+        */
+        datatail[0]             = 0x80;
+        datatail[tailbytes - 8] = (uint8) (databits >> 56 & 0xff);
+        datatail[tailbytes - 7] = (uint8) (databits >> 48 & 0xff);
+        datatail[tailbytes - 6] = (uint8) (databits >> 40 & 0xff);
+        datatail[tailbytes - 5] = (uint8) (databits >> 32 & 0xff);
+        datatail[tailbytes - 4] = (uint8) (databits >> 24 & 0xff);
+        datatail[tailbytes - 3] = (uint8) (databits >> 16 & 0xff);
+        datatail[tailbytes - 2] = (uint8) (databits >>  8 & 0xff);
+        datatail[tailbytes - 1] = (uint8) (databits >>  0 & 0xff);
+
+        // Process each 512-bit chunk
+        for (uint32 lidx = 0; lidx < loopcount; lidx++)
+        {
+            // Compute all elements in W
+            memset (W, 0, 80 * sizeof (uint32));
+
+            // Break 512-bit chunk into sixteen 32-bit, big endian words
+            for (uint32 widx = 0; widx <= 15; widx++)
+            {
+                auto wcount = 24;
+                auto didx = 0;
+
+                // Copy byte-per byte from specified buffer
+                while (didx < numBytes && wcount >= 0)
+                {
+                    W[widx] += ((((uint32*) data)[didx]) << wcount);
+                    ++didx;
+                    wcount -= 8;
+                }
+
+                // Fill out W with padding as needed
+                while (wcount >= 0)
+                {
+                    W[widx] += (((uint32) datatail[didx - numBytes]) << wcount);
+                    ++didx;
+                    wcount -= 8;
+                }
+            }
+
+            /** Extend the sixteen 32-bit words into eighty 32-bit words, with potential optimization from:
+                "Improving the Performance of the Secure Hash Algorithm (SHA-1)" by Max Locktyukhin
+            */
+            for (uint32 widx = 16; widx <= 31; widx++)
+                W[widx] = std::rotl ((W[widx - 3] ^ W[widx - 8] ^ W[widx - 14] ^ W[widx - 16]), 1);
+
+            for (uint32 widx = 32; widx <= 79; widx++)
+                W[widx] = std::rotl ((W[widx - 6] ^ W[widx - 16] ^ W[widx - 28] ^ W[widx - 32]), 2);
+
+            // Main loop
+            a = state[0];
+            b = state[1];
+            c = state[2];
+            d = state[3];
+            e = state[4];
+
+            for (uint32 idx = 0; idx <= 79; idx++)
+            {
+                uint32 f = 0;
+
+                if (idx <= 19)
+                {
+                    f = (b & c) | ((~b) & d);
+                    k = 0x5a827999;
+                }
+                else if (idx >= 20 && idx <= 39)
+                {
+                    f = b ^ c ^ d;
+                    k = 0x6ed9eba1;
+                }
+                else if (idx >= 40 && idx <= 59)
+                {
+                    f = (b & c) | (b & d) | (c & d);
+                    k = 0x8f1bbcdc;
+                }
+                else if (idx >= 60 && idx <= 79)
+                {
+                    f = b ^ c ^ d;
+                    k = 0xca62c1d6;
+                }
+
+                temp = std::rotl (a, 5) + f + e + k + W[idx];
+                e = d;
+                d = c;
+                c = std::rotl (b, 30);
+                b = a;
+                a = temp;
+            }
+
+            state[0] += a;
+            state[1] += b;
+            state[2] += c;
+            state[3] += d;
+            state[4] += e;
+        }
+
+#if 0
+        /* Store binary digest in supplied buffer */
+        if (data)
+        {
+            for (idx = 0; idx < 5; idx++)
+            {
+                digest[idx * 4 + 0] = (uint8) (state[idx] >> 24);
+                digest[idx * 4 + 1] = (uint8) (state[idx] >> 16);
+                digest[idx * 4 + 2] = (uint8) (state[idx] >> 8);
+                digest[idx * 4 + 3] = (uint8) (state[idx]);
+            }
+        }
+#endif
     }
 
     void processFinalBlock (const void* data, uint32 numBytes) noexcept
     {
-        jassert (numBytes < 64);
+        jassert (numBytes < 20);
 
         length += numBytes;
         length *= 8; // (the length is stored as a count of bits, not bytes)
 
-        uint8 finalBlocks[128];
+        uint8 finalBlocks[20];
 
         memcpy (finalBlocks, data, numBytes);
-        finalBlocks[numBytes++] = 128; // append a '1' bit
+        finalBlocks[numBytes++] = 20; // append a '1' bit
 
         while (numBytes != 56 && numBytes < 64 + 56)
             finalBlocks[numBytes++] = 0; // pad with zeros..
@@ -87,152 +206,38 @@ struct SHA1Processor
         for (int i = 8; --i >= 0;)
             finalBlocks[numBytes++] = (uint8) (length >> (i * 8)); // append the length.
 
-        jassert (numBytes == 64 || numBytes == 128);
+        jassert (numBytes == 20);
 
-        processFullBlock (finalBlocks);
+        processFullBlock (finalBlocks, 20);
 
-        if (numBytes > 64)
-            processFullBlock (finalBlocks + 64);
+        if (numBytes > 20)
+            processFullBlock (finalBlocks + 20, numBytes - 20);
     }
-
-    void copyResult (uint8* result) const noexcept
-    {
-        for (const auto s : state)
-        {
-            *result++ = (uint8) (s >> 24);
-            *result++ = (uint8) (s >> 16);
-            *result++ = (uint8) (s >> 8);
-            *result++ = (uint8) s;
-        }
-    }
-
-    void processStream (InputStream& input, int64_t numBytesToRead, uint8* result)
-    {
-        if (numBytesToRead < 0)
-            numBytesToRead = std::numeric_limits<int64_t>::max();
-
-        for (;;)
-        {
-            uint8 buffer[64];
-            auto bytesRead = input.read (buffer, (int) std::min (numBytesToRead, (int64_t) sizeof (buffer)));
-
-            if (bytesRead < (int) sizeof (buffer))
-            {
-                processFinalBlock (buffer, (unsigned int) bytesRead);
-                break;
-            }
-
-            numBytesToRead -= (int64_t) sizeof (buffer);
-            processFullBlock (buffer);
-        }
-
-        copyResult (result);
-    }
-
-private:
-    uint32 state[5];
-    uint64 length = 0;
-
-    static inline uint32 rotateLeft (uint32 x, uint32 y) noexcept { return (x << y) | (x >> (32 - y)); }
 };
 
 //==============================================================================
-SHA1::SHA1 (const MemoryBlock& data)
-{
-    process (data.getData(), data.getSize());
-}
-
-SHA1::SHA1 (const void* data, size_t numBytes)
-{
-    process (data, numBytes);
-}
-
-SHA1::SHA1 (InputStream& input, int64 numBytesToRead)
-{
-    SHA1Processor processor;
-    processor.processStream (input, numBytesToRead, result);
-}
+SHA1::SHA1()                                            { result.fill (0); }
+SHA1::SHA1 (const MemoryBlock& data)                    { process (data.getData(), data.getSize()); }
+SHA1::SHA1 (const void* data, size_t numBytes)          { process (data, numBytes); }
+SHA1::SHA1 (InputStream& input, int64 numBytesToRead)   { result = SHA1Processor().processStream (input, numBytesToRead); }
+SHA1::SHA1 (CharPointer_UTF8 utf8) noexcept             { process (utf8.getAddress(), utf8.sizeInBytes() - 1); }
+MemoryBlock SHA1::getRawData() const                    { return MemoryBlock (result.data(), result.size()); }
+String SHA1::toHexString() const                        { return String::toHexString (result.data(), (int) result.size(), 0); }
 
 SHA1::SHA1 (const File& file)
 {
     FileInputStream fin (file);
 
     if (fin.getStatus().wasOk())
-    {
-        SHA1Processor processor;
-        processor.processStream (fin, -1, result);
-    }
+        result = SHA1Processor().processStream (fin, -1);
     else
-    {
         zerostruct (result);
-    }
-}
-
-SHA1::SHA1 (CharPointer_UTF8 utf8) noexcept
-{
-    jassert (utf8.getAddress() != nullptr);
-    process (utf8.getAddress(), utf8.sizeInBytes() - 1);
 }
 
 void SHA1::process (const void* data, size_t numBytes)
 {
+    jassert (data != nullptr && numBytes > 0);
+
     MemoryInputStream m (data, numBytes, false);
-    SHA1Processor processor;
-    processor.processStream (m, -1, result);
+    result = SHA1Processor().processStream (m, -1);
 }
-
-MemoryBlock SHA1::getRawData() const
-{
-    return MemoryBlock (result, sizeof (result));
-}
-
-String SHA1::toHexString() const
-{
-    return String::toHexString (result, sizeof (result), 0);
-}
-
-bool SHA1::operator== (const SHA1& other) const noexcept  { return memcmp (result, other.result, sizeof (result)) == 0; }
-bool SHA1::operator!= (const SHA1& other) const noexcept  { return ! operator== (other); }
-
-//==============================================================================
-#if SQUAREPINE_COMPILE_UNIT_TESTS
-
-class SHA1Tests final : public UnitTest
-{
-public:
-    SHA1Tests() :
-        UnitTest ("SHA-1", UnitTestCategories::cryptography)
-    {
-    }
-
-    void test (const char* input, const char* expected)
-    {
-        {
-            SHA1 hash (input, strlen (input));
-            expectEquals (hash.toHexString(), String (expected));
-        }
-
-        {
-            CharPointer_UTF8 utf8 (input);
-            SHA1 hash (utf8);
-            expectEquals (hash.toHexString(), String (expected));
-        }
-
-        {
-            MemoryInputStream m (input, strlen (input), false);
-            SHA1 hash (m);
-            expectEquals (hash.toHexString(), String (expected));
-        }
-    }
-
-    void runTest() override
-    {
-        // beginTest ("SHA1");
-
-        // test ("", "da39a3ee5e6b4b0d3255bfef95601890afd80709");
-        // test ("The quick brown fox jumps over the lazy dog",  "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12");
-        // test ("The quick brown fox jumps over the lazy dog.", "408d94384216f890ff7a0c3528e8bed1e0b01621");
-    }
-};
-
-#endif
--- modules/squarepine_cryptography/hash/SHA1.h
+++ modules/squarepine_cryptography/hash/SHA1.h
@@ -1,9 +1,12 @@
-/** SHA-1 secure hash generator.
+/** SHA-1 hash generator.
 
-    Create one of these objects from a block of source data or a stream, and it
-    calculates the SHA-1 hash of that data.
+    Create one of these objects from a block of source data or a stream,
+    and it calculates the SHA-1 hash of that data.
 
-    You can retrieve the hash as a raw 32-byte block, or as a 64-digit hex string.
+    You can retrieve the hash as its raw 20-byte block or as a 40-digit hex string.
+
+    @see https://github.com/vog/sha1/blob/master/sha1.hpp
+    @see https://github.com/CTrabant/teeny-sha1/blob/main/teeny-sha1.c
 */
 class SHA1 final
 {
@@ -12,18 +15,21 @@ public:
         The default constructor just creates a hash filled with zeros.
         (This is not equal to the hash of an empty block of data).
     */
-    SHA1() = default;
+    SHA1();
 
-    /** Destructor. */
-    ~SHA1() = default;
+    /** Creates a checksum from a UTF-8 buffer.
+        Example:
+        @code
+            SHA1 checksum (myString.toUTF8());
+        @endcode
+    */
+    explicit SHA1 (CharPointer_UTF8 utf8Text) noexcept;
 
-    /** Creates a copy of another SHA1. */
-    SHA1 (const SHA1&) = default;
+    /** Reads a file and generates the hash of its contents.
+        If the file can't be opened, the hash will be left uninitialised (i.e. full of zeros).
+    */
+    explicit SHA1 (const File& file);
 
-    /** Copies another SHA1. */
-    SHA1& operator= (const SHA1&) = default;
-
-    //==============================================================================
     /** Creates a hash from a block of raw data. */
     explicit SHA1 (const MemoryBlock& data);
 
@@ -38,34 +44,39 @@ public:
     */
     SHA1 (InputStream& input, int64 maxBytesToRead = -1);
 
-    /** Reads a file and generates the hash of its contents.
-        If the file can't be opened, the hash will be left uninitialised (i.e. full of zeros).
-    */
-    explicit SHA1 (const File& file);
+    /** Creates a copy of another SHA1. */
+    SHA1 (const SHA1&) = default;
+    /** Creates a copy of another SHA1. */
+    SHA1 (SHA1&&) = default;
 
-    /** Creates a checksum from a UTF-8 buffer.
-        Example:
-        @code
-            SHA1 checksum (myString.toUTF8());
-        @endcode
-    */
-    explicit SHA1 (CharPointer_UTF8 utf8Text) noexcept;
+    /** Destructor. */
+    ~SHA1() = default;
 
     //==============================================================================
-    /** @returns the hash as a 32-byte block of data. */
+    /** Copies another SHA1. */
+    SHA1& operator= (const SHA1&) = default;
+    /** Copies another SHA1. */
+    SHA1& operator= (SHA1&&) = default;
+
+    /** @returns */
+    bool operator== (const SHA1&) const = default;
+    /** @returns */
+    bool operator!= (const SHA1&) const = default;
+
+    //==============================================================================
+    /** @returns the hash as a 20-byte block of data. */
     MemoryBlock getRawData() const;
 
-    /** @returns the checksum as a 64-digit hex string. */
+    /** @returns the checksum as a 40-digit hex string. */
     String toHexString() const;
-
-    //==============================================================================
-    bool operator== (const SHA1&) const noexcept;
-    bool operator!= (const SHA1&) const noexcept;
 
 private:
     //==============================================================================
-    uint8 result[16] = {};
+    std::array<uint8, 20> result;
+
+    //==============================================================================
     void process (const void*, size_t);
 
+    //==============================================================================
     JUCE_LEAK_DETECTOR (SHA1)
 };
