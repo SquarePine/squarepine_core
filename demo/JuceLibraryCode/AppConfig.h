53a12f652ee08f32758dd7a8c6b757bed915c1a9
Joël R. Langlois <joel.r.langlois@gmail.com>
2025-09-17 22:56:08 -0400

    Latest JUCE updates.

--- demo/JuceLibraryCode/AppConfig.h
+++ demo/JuceLibraryCode/AppConfig.h
@@ -20,34 +20,35 @@
 
 // [END_USER_CODE_SECTION]
 
-#define JUCE_PROJUCER_VERSION 0x80009
+#define JUCE_PROJUCER_VERSION 0x8000a
 
 //==============================================================================
-#define JUCE_MODULE_AVAILABLE_juce_analytics               1
-#define JUCE_MODULE_AVAILABLE_juce_animation               1
-#define JUCE_MODULE_AVAILABLE_juce_audio_basics            1
-#define JUCE_MODULE_AVAILABLE_juce_audio_devices           1
-#define JUCE_MODULE_AVAILABLE_juce_audio_formats           1
-#define JUCE_MODULE_AVAILABLE_juce_audio_processors        1
-#define JUCE_MODULE_AVAILABLE_juce_audio_utils             1
-#define JUCE_MODULE_AVAILABLE_juce_box2d                   1
-#define JUCE_MODULE_AVAILABLE_juce_core                    1
-#define JUCE_MODULE_AVAILABLE_juce_cryptography            1
-#define JUCE_MODULE_AVAILABLE_juce_data_structures         1
-#define JUCE_MODULE_AVAILABLE_juce_dsp                     1
-#define JUCE_MODULE_AVAILABLE_juce_events                  1
-#define JUCE_MODULE_AVAILABLE_juce_graphics                1
-#define JUCE_MODULE_AVAILABLE_juce_gui_basics              1
-#define JUCE_MODULE_AVAILABLE_juce_gui_extra               1
-#define JUCE_MODULE_AVAILABLE_juce_opengl                  1
-#define JUCE_MODULE_AVAILABLE_juce_osc                     1
-#define JUCE_MODULE_AVAILABLE_juce_product_unlocking       1
-#define JUCE_MODULE_AVAILABLE_juce_video                   1
-#define JUCE_MODULE_AVAILABLE_squarepine_animation         1
-#define JUCE_MODULE_AVAILABLE_squarepine_audio             1
-#define JUCE_MODULE_AVAILABLE_squarepine_core              1
-#define JUCE_MODULE_AVAILABLE_squarepine_cryptography      1
-#define JUCE_MODULE_AVAILABLE_squarepine_graphics          1
+#define JUCE_MODULE_AVAILABLE_juce_analytics                      1
+#define JUCE_MODULE_AVAILABLE_juce_animation                      1
+#define JUCE_MODULE_AVAILABLE_juce_audio_basics                   1
+#define JUCE_MODULE_AVAILABLE_juce_audio_devices                  1
+#define JUCE_MODULE_AVAILABLE_juce_audio_formats                  1
+#define JUCE_MODULE_AVAILABLE_juce_audio_processors               1
+#define JUCE_MODULE_AVAILABLE_juce_audio_processors_headless      1
+#define JUCE_MODULE_AVAILABLE_juce_audio_utils                    1
+#define JUCE_MODULE_AVAILABLE_juce_box2d                          1
+#define JUCE_MODULE_AVAILABLE_juce_core                           1
+#define JUCE_MODULE_AVAILABLE_juce_cryptography                   1
+#define JUCE_MODULE_AVAILABLE_juce_data_structures                1
+#define JUCE_MODULE_AVAILABLE_juce_dsp                            1
+#define JUCE_MODULE_AVAILABLE_juce_events                         1
+#define JUCE_MODULE_AVAILABLE_juce_graphics                       1
+#define JUCE_MODULE_AVAILABLE_juce_gui_basics                     1
+#define JUCE_MODULE_AVAILABLE_juce_gui_extra                      1
+#define JUCE_MODULE_AVAILABLE_juce_opengl                         1
+#define JUCE_MODULE_AVAILABLE_juce_osc                            1
+#define JUCE_MODULE_AVAILABLE_juce_product_unlocking              1
+#define JUCE_MODULE_AVAILABLE_juce_video                          1
+#define JUCE_MODULE_AVAILABLE_squarepine_animation                1
+#define JUCE_MODULE_AVAILABLE_squarepine_audio                    1
+#define JUCE_MODULE_AVAILABLE_squarepine_core                     1
+#define JUCE_MODULE_AVAILABLE_squarepine_cryptography             1
+#define JUCE_MODULE_AVAILABLE_squarepine_graphics                 1
 
 #define JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED 1
 
@@ -56,6 +57,10 @@
 
 #ifndef    JUCE_USE_WINRT_MIDI
  #define   JUCE_USE_WINRT_MIDI 1
+#endif
+
+#ifndef    JUCE_USE_WINDOWS_MIDI_SERVICES
+ //#define JUCE_USE_WINDOWS_MIDI_SERVICES 0
 #endif
 
 #ifndef    JUCE_ASIO
@@ -76,10 +81,6 @@
 
 #ifndef    JUCE_JACK
  //#define JUCE_JACK 0
-#endif
-
-#ifndef    JUCE_BELA
- //#define JUCE_BELA 0
 #endif
 
 #ifndef    JUCE_USE_ANDROID_OBOE
